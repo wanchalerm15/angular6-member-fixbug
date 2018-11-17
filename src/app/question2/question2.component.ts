0738377f728d82e5d0d4ccf599ca8817f7428bbb
WanchaloemMacBookPro <wanchaloemmacbookpro@WanchaloemMacBookPros-MacBook-Pro.local>
2018-11-17 19:32:12 +0700

    update ckeditor item

--- src/app/question2/question2.component.ts
+++ src/app/question2/question2.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+declare const ClassicEditor;
 @Component({
-  selector: 'app-question2',
-  templateUrl: './question2.component.html',
-  styleUrls: ['./question2.component.css']
+    selector: 'app-question2',
+    templateUrl: './question2.component.html',
+    styleUrls: ['./question2.component.css']
 })
 export class Question2Component implements OnInit {
+    editor: any;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        ClassicEditor
+            .create(document.getElementById('editor'))
+            .then(editor => this.editor = editor)
+            .catch(error => console.log(error));
+    }
 
+    // บันทึกข้อมูล
+    onSubmit() {
+        console.log(this.editor.getData());
+    }
 }
